9a07e3f6d6e2372773e66214b24561022bd0e93c
HariomGurjar <hariom.gurjar85@gmail.com>
2021-09-03 14:40:21 +0530

    

---
+++ BST/BST.cpp
@@ -0,0 +1,67 @@
+//Binary search tree Build and print 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//ode class for BST
+
+class Node{
+  public:
+  
+   int data;
+   Node*left;
+   Node*right;
+   
+   Node(int data){
+       this->data=data;
+       left=NULL;
+       right=NULL;
+   }
+   
+};
+
+// insert any data into binary search tree
+
+Node* insertInBST(int data,Node*root){
+    if(root==NULL)
+      {
+         Node*node= new Node(data);
+         return node;
+      }
+     
+     if(data<root->data)
+      root->left=insertInBST(data,root->left);
+     else
+      root->right=insertInBST(data,root->right);
+      
+     return root;
+}
+
+
+// inorder print of BST (result in sorted data)
+
+void inOrder(Node* root){
+    if(root==NULL)
+     return ;
+    
+    inOrder(root->left);
+    cout<<root->data<<" ";
+    inOrder(root->right);
+}
+
+Node* bulidBST(){
+    int x;
+    cin>>x;
+    Node* root=NULL;
+    while(x!=-1){
+      root= insertInBST(x,root);
+        cin>>x;
+    }
+    return root;
+}
+
+int main(){
+    Node* root;
+    root=bulidBST();
+    inOrder(root);
+}
